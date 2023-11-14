887ed247ef2c510c432001d9d93287cef138ffcd
Kevin Li <99510924+kevin200301@users.noreply.github.com>
2023-11-14 02:04:33 -0500

    added minimum bounce angle

--- game/ball.gd
+++ game/ball.gd
@@ -17,6 +17,21 @@ func initialize(start_position : Vector2, killer_rid : RID):
 func _on_body_shape_exited(body_rid, body, _body_shape_index, _local_shape_index):
 	if body_rid != killer:
 		var direction = linear_velocity.angle()
+		
+		if direction > 0 && direction < 0.5:
+			direction = 0.5
+			print("change1")
+		elif direction < 0 && direction > -0.5:
+			direction = -0.5
+			print("change2")
+		elif direction > 2.5:
+			direction = 2.5
+			print("change3")
+		elif direction < -2.5:
+			direction = -2.5
+			print("change4")
+		print(direction)
+		
 		linear_velocity = Vector2(speed, 0).rotated(direction)
 
 		if body.has_method("lose_health"):
