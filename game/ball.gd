c1a0c2f33ed7c697bf59deaada71f74cacdd6d2d
corrupthexagons <d4rkM4g3r@gmail.com>
2023-11-14 11:58:10 -0500

    Added soft lower limit to horizontal bounce angle

--- game/ball.gd
+++ game/ball.gd
@@ -16,21 +16,16 @@ func initialize(start_position : Vector2, killer_rid : RID):
 
 func _on_body_shape_exited(body_rid, body, _body_shape_index, _local_shape_index):
 	if body_rid != killer:
-		var direction = linear_velocity.angle()
+		var direction : float = linear_velocity.angle()
+		var negative : bool = direction < 0
 		
-		if direction > 0 && direction < 0.5:
-			direction = 0.5
-			print("change1")
-		elif direction < 0 && direction > -0.5:
-			direction = -0.5
-			print("change2")
-		elif direction > 2.5:
-			direction = 2.5
-			print("change3")
-		elif direction < -2.5:
-			direction = -2.5
-			print("change4")
-		print(direction)
+		direction = abs(direction)
+		var upper = (0.5 * direction) + ((PI-0.5) * 0.5)
+		var lower = (0.5 * direction) + 0.25
+
+		direction = min(max(direction, lower), upper)
+		if negative:
+			direction *= -1
 		
 		linear_velocity = Vector2(speed, 0).rotated(direction)
 
